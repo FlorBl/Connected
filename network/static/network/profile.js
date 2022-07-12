46595e40ed1c5238c1038e79da4e9766854c90d4
FlorBl <florjanblakaj@hotmail.com>
2022-07-12 08:13:13 -0400

    Changes

--- network/static/network/profile.js
+++ network/static/network/profile.js
@@ -3,20 +3,23 @@
 
 // Register Form Popup
 function editProfile(){
-
+    var profile = document.getElementById('ProfileImage');
     var lightbox = document.getElementById('profileEdit');
 
     lightbox.style.visibility = 'hidden';
 
     dimmer = document.createElement("div");
-    
     dimmer.style.width =  window.innerWidth + 'px';
     dimmer.style.height = window.innerHeight + 'px';
     dimmer.className = 'dimmer';
     
+
+
     dimmer.onclick = function(){
         document.body.removeChild(this);   
         lightbox.style.visibility = 'hidden';
+    profile.style.visibility = 'visible';
+
 
     }
 
@@ -24,13 +27,20 @@ function editProfile(){
     x.onclick = function(){
         dimmer.className = '';  
         lightbox.style.visibility = 'hidden';
+    profile.style.visibility = 'visible';
+
 
     }
 
     document.body.appendChild(dimmer);
-    
+    profile.style.visibility = 'hidden';
     lightbox.style.visibility = 'visible';
     return false;
 }
 
 
+
+function HideProfilePic(){
+    var profile = document.getElementById('ProfileImage');
+    profile.style.visibility = 'hidden';
+}
