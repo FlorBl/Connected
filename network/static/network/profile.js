eda3bb1636f2749aaff3b953b49a904c38f280f2
FlorBl <florjanblakaj@hotmail.com>
2022-07-20 18:06:33 -0400

    Connected to AWS via CLI

--- network/static/network/profile.js
+++ network/static/network/profile.js
@@ -44,3 +44,42 @@ function HideProfilePic(){
     var profile = document.getElementById('ProfileImage');
     profile.style.visibility = 'hidden';
 }
+
+
+
+
+function postsForm(){
+     var profile = document.getElementById('ProfileImage');
+    var lightbox = document.getElementById('popPostForm');
+
+    lightbox.style.visibility = 'hidden';
+
+    dimmer = document.createElement("div");
+    dimmer.style.width =  window.innerWidth + 'px';
+    dimmer.style.height = window.innerHeight + 'px';
+    dimmer.className = 'dimmer';
+    
+
+
+    dimmer.onclick = function(){
+        document.body.removeChild(this);   
+        lightbox.style.visibility = 'hidden';
+    profile.style.visibility = 'visible';
+
+
+    }
+
+    var x = document.getElementById('x22');
+    x.onclick = function(){
+        dimmer.className = '';  
+        lightbox.style.visibility = 'hidden';
+    profile.style.visibility = 'visible';
+
+
+    }
+
+    document.body.appendChild(dimmer);
+    profile.style.visibility = 'hidden';
+    lightbox.style.visibility = 'visible';
+    return false;
+}
