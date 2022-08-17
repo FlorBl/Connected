51eff786e266c141f43c52334348153c62f61d31
FlorBl <florjanblakaj@hotmail.com>
2022-08-17 14:36:56 -0400

    ..

--- project4/settings.py
+++ project4/settings.py
@@ -170,7 +170,7 @@ AWS_STORAGE_BUCKET_NAME = 'django-greensky-bucket'
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_STORAGE = '{project4}.storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'project4.storages.backends.s3boto3.S3Boto3Storage'
 
 ## different
 AWS_LOCATION = 'static'
