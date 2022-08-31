62f652a00079045b6cd911f10e46b47d2ccfc101
FlorBl <florjanblakaj@hotmail.com>
2022-08-31 15:15:57 -0400

    .....; git

--- project4/settings.py
+++ project4/settings.py
@@ -14,7 +14,7 @@ import os
 import django_on_heroku
 import dj_database_url
 from decouple import config
-from .aws import conf
+from aws.conf import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
@@ -145,6 +145,7 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # until here
 ''''''
+'''
 django_on_heroku.settings(locals(), staticfiles=False)
 #S3 BUCKETS CONFIG (KEY_ID, SECRET_KEY, SHOULD ALL BE HIDDEN! )
 AWS_S3_HOST = 's3.ca-central-1.amazonaws.com'
@@ -158,7 +159,7 @@ AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
-
+'''
 '''
 AWS_S3_HOST = 's3.ca-central-1.amazonaws.com'
 AWS_ACCESS_KEY_ID = 'AKIAZHCVDPUKBB2LK6WD'
